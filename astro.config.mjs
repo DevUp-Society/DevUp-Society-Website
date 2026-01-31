53a420482c9600ec7304eba8e7dbfe80815b7198
FaizanMohammed07 <aliffaizan1212@gmail.com>
2026-01-31 16:40:49 +0530

    handle the working condition of the Icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false,
-    })
+    }),
+    icon(),
   ],
-  output: 'static',
-  site: 'https://devup-society.com',
+  output: "static",
+  site: "https://devup-society.com",
 });
